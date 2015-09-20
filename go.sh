8faf3fa5a306db727f2de32ecf4822ea94b7032d
Bob Stein <me@bobste.in>
2015-09-20 16:56:42 -0400

    go.sh now checks out both qiki-python and number-playground projects

--- go.sh
+++ go.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+cd /home/visibone/qiki/number-playground
 git pull origin rnsdev
+
+cd /home/visibone/qiki/qiki-python
+git pull origin rnsdev
+
 PYTHONPATH=/home/visibone/qiki/number-playground:/home/visibone/qiki/qiki-python
 python2.7 manage.py runserver 64.239.60.215:27647
