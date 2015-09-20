7e6a30bd5fb42a8141238ebd20a4f13b5e8ef804
Bob Stein <me@bobste.in>
2015-09-20 17:17:46 -0400

    exporting the path, sheesh

--- go.sh
+++ go.sh
@@ -6,5 +6,5 @@ git pull origin rnsdev
 cd /home/visibone/qiki/qiki-python
 git pull origin rnsdev
 
-PYTHONPATH=/home/visibone/qiki/number-playground:/home/visibone/qiki/qiki-python
+export PYTHONPATH=/home/visibone/qiki/number-playground:/home/visibone/qiki/qiki-python
 python2.7 manage.py runserver 64.239.60.215:27647
