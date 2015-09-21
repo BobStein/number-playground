fef4b58c3d8810485a94839a036ce9f5892124c4
Bob Stein <stein@visibone.com>
2015-09-21 09:36:59 -0400

    Don't really need export do we.  (Original problem was wrong cd.)

--- go.sh
+++ go.sh
@@ -6,5 +6,5 @@ git pull origin rnsdev
 cd /home/visibone/qiki/number-playground
 git pull origin rnsdev
 
-export PYTHONPATH=/home/visibone/qiki/number-playground:/home/visibone/qiki/qiki-python
+PYTHONPATH=/home/visibone/qiki/number-playground:/home/visibone/qiki/qiki-python
 python2.7 manage.py runserver 64.239.60.215:27647
