9ff85b9246cef2332cacf5e333e193a3cd882bfe
Bob Stein <stein@visibone.com>
2015-09-25 13:43:00 -0400

    go.sh enables its own execution bit, groan

--- go.sh
+++ go.sh
@@ -6,5 +6,7 @@ git pull origin rnsdev
 cd /home/visibone/qiki/number-playground
 git pull origin rnsdev
 
+chmod +x go.sh
+
 export PYTHONPATH=/home/visibone/qiki/number-playground:/home/visibone/qiki/qiki-python
 python2.7 manage.py runserver 64.239.60.215:27647 --insecure
