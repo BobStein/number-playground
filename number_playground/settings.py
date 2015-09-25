4cb1fe6794f12c4c831e016074109a1f55d8f1d2
Bob Stein <me@bobste.in>
2015-09-25 19:50:35 -0400

    Site 3 to 5.

--- number_playground/settings.py
+++ number_playground/settings.py
@@ -89,7 +89,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-SITE_ID = 3
+SITE_ID = 5
 # thanks http://stackoverflow.com/questions/29871973/error-migrating-comment-framework-into-django#answer-29872491
 # thanks https://docs.djangoproject.com/en/1.8/ref/contrib/sites/#django.contrib.sites.models.Site
 
