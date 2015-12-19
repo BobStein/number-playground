12409304f19313a136468ad790a24dfb4bc34ab8
Bob Stein <me@bobste.in>
2015-12-19 10:27:13 -0500

    qoolbar.js beginnings - need to deply static files some effing how.

---
+++ number_playground/static/number_playground/qoolbar.js
@@ -0,0 +1,13 @@
+// qoolbar.js - a menu of qiki verb icons to apply to things on a web page.
+
+var qoolbar = (function() {
+    var qb = {};
+
+    qb.html = function(options) {
+        return "<div>qoolbar goes here</div>";
+    };
+
+    return qb;
+}());
+
+// THANKS:  http://www.adequatelygood.com/JavaScript-Module-Pattern-In-Depth.html
