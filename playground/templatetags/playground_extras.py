127012eb7f57ba831d2f9669ee06ecef65cd1374
Bob Stein <me@bobste.in>
2016-03-16 19:27:09 -0400

    Fixed sbj_idn.qstring in qiki playground, taking advantage of word.sbj being itself a word. qiki playground can hide icon history or show whn code. Not sure why word.spawn(idn) didn't work though. Number.INFINITESIMAL Unit tests for Number.is_positive(), is_zero(), is_negative(). PyCharm inconsistent when e.g. noinspection PyUnresolvedReferences needed for Number.POSITIVE_INFINITY

--- playground/templatetags/playground_extras.py
+++ playground/templatetags/playground_extras.py
@@ -59,9 +59,6 @@ def icon_diagram(vrb, icon_entry):
 
 @register.inclusion_tag('word-diagram-call.html')
 def word_diagram(word, show_idn=False):
-    sbj = word.spawn(word.sbj)
-    vrb = word.spawn(word.vrb)
-    obj = word.spawn(word.obj)
     if word.is_defined():
         if word.is_a_verb():
             is_a_what = "verb"
@@ -92,25 +89,25 @@ def word_diagram(word, show_idn=False):
             icon_entry[q.sbj] = author_entry
         author_entry['history'].append(q)
         author_entry['num'] = q.num
-    obj_txt = obj.txt
+    obj_txt = word.obj.txt
     if obj_txt == '':
-        obj_txt = "Word {}".format(render_num(obj.idn))
+        obj_txt = "Word {}".format(render_num(word.obj.idn))
         # TODO:  This will have to be smarter.  Comment objects shouldn't be identified by txt alone.  Arrows??
     return dict(
         show_idn=show_idn,
         idn=render_num(word.idn),
         idn_qstring=word.idn.qstring(underscore=1),
-        sbj_txt=sbj.txt,
-        vrb_txt=vrb.txt,
+        sbj_idn=word.sbj.idn.qstring(),
+        sbj_txt=word.sbj.txt,
+        vrb_idn=word.vrb.idn.qstring(),
+        vrb_txt=word.vrb.txt,
+        obj_idn=word.obj.idn.qstring(),
         obj_txt=obj_txt,
         txt=word.txt,
         num=render_num(word.num),
         num_qstring=word.num.qstring(),
         is_a_what=is_a_what,
         yyyy_mmdd_hhmm_ss_mmm=time_code,
-        sbj_idn=sbj.idn.qstring(),
-        vrb_idn=vrb.idn.qstring(),
-        obj_idn=obj.idn.qstring(),
         jbo=word.jbo,
         jbo_report=jbo_report,
         jbo_dict=jbo_dict,
