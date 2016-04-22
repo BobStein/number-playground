805ba3516851730086d9b64efe88c6a854498ec6
Bob Stein <me@bobste.in>
2016-04-22 13:07:10 -0400

    Fixed bug in Lex.find_words() where returned words[i].jbo[j].txt was a Number instead of a Text. Another step towards using the jbo_vrb feature of find_words() for constructing the qoolbar.

--- playground/views.py
+++ playground/views.py
@@ -229,7 +229,16 @@ def qiki_ajax(request):
                     return valid_response('report', report)
                 elif action == 'qoolbar_list':
                     verbs = qoolbar.get_verbs()
-                    return valid_response('verbs', verbs)
+                    # TODO:  Make Word json serializable, http://stackoverflow.com/a/3768975/673991
+                    # Then we wouldn't have to translate verbs to verb_dicts:
+                    verb_dicts = []
+                    for verb in verbs:
+                        verb_dicts.append(dict(
+                            idn=verb.idn.qstring(),
+                            name=verb.txt,
+                            icon_url=verb.icon_url
+                        ))
+                    return valid_response('verbs', verb_dicts)
                     # Used by:
                     #     number_playground/playground/static/qoolbar.js
                     #     number_playground/playground/templatetags/playground_extras.py ?
